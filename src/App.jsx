93a74be37a03f9f8de665d2feae8eb6ab4c7c83b
sumer singh <santurajpurphit@gmail.com>
2024-06-29 18:05:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+
+
+export default function App() {
+  return (
+    <div className="bg-customWhite ">
+     <Navbar/>
+      <Home/>
+      
+    </div>
+  )
+}
